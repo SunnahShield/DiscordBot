3a0a7dd5beadb030741eb71e2acc9ce161f94d89
Abdelaziz Sarhan <s-abdel.abdelaziz@zewailcity.edu.eg>
2026-06-24 10:23:45 +0300

    Allow global slash command deployment

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -5,18 +5,24 @@ const { commands } = require('./commands');
 
 const { CLIENT_ID, DISCORD_TOKEN, GUILD_ID } = process.env;
 
-if (!CLIENT_ID || !DISCORD_TOKEN || !GUILD_ID) {
-  throw new Error('CLIENT_ID, DISCORD_TOKEN, and GUILD_ID must be set in .env');
+if (!CLIENT_ID || !DISCORD_TOKEN) {
+  throw new Error('CLIENT_ID and DISCORD_TOKEN must be set in .env');
 }
 
 const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
 
 async function deployCommands() {
-  await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+  const route = GUILD_ID
+    ? Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID)
+    : Routes.applicationCommands(CLIENT_ID);
+
+  await rest.put(route, {
     body: commands.map((command) => command.toJSON()),
   });
 
-  console.log('Slash commands registered.');
+  console.log(
+    GUILD_ID ? 'Slash commands registered for the guild.' : 'Global slash commands registered.'
+  );
 }
 
 module.exports = {
