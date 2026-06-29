b6e2d4df6ff99740f5a910f2fc13ac8cffe34437
SunnahShield <sunnahshield1@gmail.com>
2026-06-29 10:34:53 +0300

    Enhance command registration with localizations
    Added localization support for punishment commands.

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { REST, Routes } = require('discord.js');
-const { commands } = require('./commands');
+const { commands, punishmentLocalizations } = require('./commands');
 
 const { CLIENT_ID, DISCORD_TOKEN, GUILD_ID } = process.env;
 
@@ -17,7 +17,17 @@ async function deployCommands() {
     : Routes.applicationCommands(CLIENT_ID);
 
   await rest.put(route, {
-    body: commands.map((command) => command.toJSON()),
+body: commands.map((command) => {
+      const json = command.toJSON();
+      const localizations = punishmentLocalizations[json.name];
+
+      if (localizations) {
+        json.name_localizations = localizations.name_localizations;
+        json.description_localizations = localizations.description_localizations;
+      }
+
+      return json;
+    }),
   });
 
   console.log(
